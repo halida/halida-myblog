3ca9172f2c185cfbde142c41a954264e13ab7cb6
linjunhalida <linjunhalida@e9e0458a-f1e9-11dd-ac5b-dfceb64ead24>
2009-02-03 12:55:26 +0000

    debug

--- trunk/testproject/settings.py
+++ trunk/testproject/settings.py
@@ -76,6 +76,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'accounts',
-    'myblog',
+    'testproject.accounts',
+    'testproject.myblog',
 )
